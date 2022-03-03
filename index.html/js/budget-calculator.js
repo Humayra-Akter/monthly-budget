29729830265a94836b61b3bb37a1dcac3ec05fb2
Humayra Akter <humayraakter25@gmail.com>
2022-03-03 21:43:30 +0600

    forth commit

--- index.html/js/budget-calculator.js
+++ index.html/js/budget-calculator.js
@@ -5,12 +5,24 @@ function updateExpense(product) {
     if (productExpense > 0) {
         totalCost(productExpense);
     }
+    else {
+        const failError = document.getElementById('notify-fail');
+        failError.style.display = 'block';
+        totalCost();
+    }
+    //productInput.value = '';
 };
 let sum = 0;
 function totalCost(productExpense) {
     sum = sum + productExpense;
     return sum;
+};
+function finalBalance(incomeInput) {
+    totalCost();
+    const balance = incomeInput - sum;
+    return balance;
 }
+//calling input function
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
     updateExpense('food');
@@ -22,7 +34,20 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     updateExpense('clothes');
 });
 document.getElementById('calculate-btn').addEventListener('click', function () {
+
+});
+document.getElementById('calculate-btn').addEventListener('click', function () {
     // calculating total expense
     const totalExpense = document.getElementById('total-expense');
     totalExpense.value = sum;
+});
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInputField = document.getElementById('income-input-field');
+    const incomeInputText = incomeInputField.value;
+    const incomeInput = parseInt(incomeInputText);
+    if (incomeInput > 0) {
+        finalBalance(incomeInput);
+        const balanceField = document.getElementById('balance-field');
+        balanceField.value = balance;
+    }
 });
